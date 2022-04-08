b02b5d0daee776fc63c84444a81fb713028592d8
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 14:59:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Table(int num)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num+"X"+i+"="+(num*i));
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Input a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Table(num);
+        
+
+        }
+    }
+}
